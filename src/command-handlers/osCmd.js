fc07e1159a737cf359569d4fc2fd5037b465b83c
NickUA <246246s@gmail.com>
2023-06-26 23:07:13 +0300

    refactor: os cpus change output again

--- src/command-handlers/osCmd.js
+++ src/command-handlers/osCmd.js
@@ -4,7 +4,9 @@ import path from 'node:path';
 const getFromOs = {
   EOL: os.EOL,
   cpus: ['Amount of CPUS: ' + os.availableParallelism()]
-    .concat(os.cpus().map(({ model }) => { return { model }; })),
+    .concat(os.cpus().map(({ model, speed }) => {
+      return { model, speed: (speed / 1024).toFixed(2) + 'GHz' };
+    })),
   homedir: path.resolve(os.homedir()),
   username: os.userInfo().username,
   architecture: os.arch()
